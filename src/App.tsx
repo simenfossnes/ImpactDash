58fced10d8a9f604535eda5d87988a5d756b5d10
simenfossnes <simenf13@student.uia.no>
2018-09-22 01:33:22 +0200

    Added react router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import * as React from 'react';
-import './App.css';
-import Hello from './components/Hello';
+import './containers/App.css';
+import Home from './containers/Home';
 import Dashboard from './containers/Dashboard';
-
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Hello name="Simon" enthusiasmLevel={1} />
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          <Dashboard name={'somename'}/>
-      </div>
+        <Switch>
+            <Route exact path='/dashboard' component={Dashboard}/>
+            <Route exact path='/' component={Home}/>
+        </Switch>
     );
   }
 }
