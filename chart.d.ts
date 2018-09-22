7b271006fbaf61280828e133c5f2d649fd390f63
Hans Hendrik Starkopf <hansh.starkopf@gmail.com>
2018-09-22 09:59:30 +0200

    setup recharts, add example

---
+++ chart.d.ts
@@ -0,0 +1,14 @@
+// declare module "recharts" {
+//     export const BarChart: any
+//     export const Bar: any
+//     export const Tooltip: any
+//     export const Legend: any
+//     export const Area: any
+//     export const AreaChart: any
+//     export const LineChart: any
+//     export const Line: any
+//     export const XAxis: any
+//     export const YAxis: any
+//     export const CartesianGrid: any
+//     export const ResponsiveContainer: any
+// }
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { Button } from 'antd';
+
 import Hello from '../components/Hello';
 import Main from '../components/Main/Main';
-import { Button } from 'antd';
+import Chart from '../components/Chart';
 
 import './App.css';
 
@@ -9,6 +11,7 @@ class Home extends React.Component {
     public render() {
         return (
             <Main>
+                <Chart />
                 <Button type="primary">Hello button!</Button>
                 <Hello name="Simon" enthusiasmLevel={1} />
             </Main>
