928fcc2e0b5548ea4889f7b5bb8b7b345eb9e5b9
Hans Hendrik Starkopf <hansh.starkopf@gmail.com>
2018-09-22 08:19:44 +0200

    add antd example

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import './App.css';
 import Hello from '../components/Hello';
 import logo from '../logo.svg';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
 
 class Home extends React.Component {
     public render() {
@@ -11,6 +13,7 @@ class Home extends React.Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
+                <Button>Hello button!</Button>
                 <Hello name="Simon" enthusiasmLevel={1}/>
                 <p className="App-intro">
                     To get started, edit <code>src/App.tsx</code> and save to reload.
